b5944e5745dc0e8f2b6ad229413ebc297d8c2e34
mizzky <ein.dreieck1212@icloud.com>
2017-08-08 21:39:02 +0900

    Added Consumer,Function,Predicate

--- src/other/August_03.java
+++ src/other/August_03.java
@@ -1,5 +1,9 @@
 package other;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 interface Interfacetalk{
 	public String hi(String str);
 }
@@ -10,8 +14,15 @@ public class August_03 {
 		Interfacetalk hello = name -> "こんちわ" + name ;
 		Interfacetalk bye = name -> "バイバイ" + name;
 
-		System.out.println(hello.hi("神様"));
+		Consumer<String> thanks = string -> System.out.println("Thank you " + string);
+		Function<String, String> function = name -> "Hi " + name;
+		Predicate<Integer> natural = num -> num > 0;
 
+		System.out.println(natural.test(100));
+
+		System.out.println(function.apply("TARO"));
+		thanks.accept("先生");
+		System.out.println(hello.hi("神様"));
 		System.out.println(bye.hi("世界"));
 	}
 
